864f2bc2fd3832cd48511c9c4651b6b83085cadf
Diogo Cunha <diogomaciel.cunha@gmail.com>
2024-05-15 19:01:17 -0300

    Adding polar points creator

--- package/distance/euclidean.go
+++ package/distance/euclidean.go
@@ -1,10 +1,25 @@
 package distance
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 type Point struct {
 	X float64
 	Y float64
+}
+
+func EuclidianPoint(seed uint32, r, a, b float64) Point {
+	random := rand.New(rand.NewSource(int64(seed)))
+
+	randomRadius := r * math.Sqrt(random.Float64())
+	randomAngle := random.Float64() * 2 * math.Pi
+
+	x := a + randomRadius*math.Cos(randomAngle)
+	y := b + randomRadius*math.Sin(randomAngle)
+
+	return Point{X: x, Y: y}
 }
 
 func EuclidianDistance(a Point, b Point) float64 {
--- package/distance/polar.go
+++ package/distance/polar.go
@@ -1,5 +1,18 @@
 package distance
 
-func PolarDistance(a Point, center Point, radius float64) float64 {
-	return 0
+import (
+	"math"
+	"math/rand"
+)
+
+func PolarPoint(seed uint32, r, a, b float64) Point {
+	random := rand.New(rand.NewSource(int64(seed)))
+
+	randomRadius := r * math.Sqrt(random.Float64())
+	randomAngle := random.Float64() * 2 * math.Pi
+
+	x := a + randomRadius*math.Cos(randomAngle)
+	y := b + randomRadius*math.Sin(randomAngle)
+
+	return Point{X: x, Y: y}
 }
